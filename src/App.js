7a835f16caa8d6537a7e38de787508637bb3f7d4
Akshay <akshay@nighthack.in>
2021-07-31 16:56:37 +0530

    Added all file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Profile from "./pages/profile";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const resp = await Axios.get(
+          "https://raw.githubusercontent.com/akshay8033/AuthPoliceV2/master/accounts.json"
+        );
+        const users = resp.data.accounts;
+        setUsers(users);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getUserData();
+  }, []);
+
+  return loading ? (
+    <p>Loading .....</p>
+  ) : (
+    <CookiesProvider>
+      <Router>
+        <Switch>
+          <Route path="/account/:id">
+            <Profile users={users} />
+          </Route>
+        </Switch>
+      </Router>
+    </CookiesProvider>
   );
 }
-
-export default App;
---
+++ src/pages/profile.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+
+export default function Profile({ users }) {
+  const { id } = useParams();
+  const [user, setUser] = useState("");
+  const [cookie] = useCookies(["logged_user"]);
+  const [disable, setDisable] = useState(false);
+  const [msg, setMessage] = useState("");
+
+  useEffect(() => {
+    const checkUser = users.filter((user) => user.id * 1 === id * 1);
+    setUser(checkUser[0]);
+  }, [users, id]);
+
+  const verifyUser = async () => {
+    try {
+      setDisable(true);
+      const data = {
+        name: user.name,
+        email: user.email,
+        verified: true,
+      };
+
+      const config = {
+        method: "put",
+        url: `http://localhost:3000/accounts/${user.id}`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
+      await axios(config);
+      setMessage("Verification request send. Will verify & get updated soon");
+    } catch (err) {
+      console.log("Error");
+      setDisable(false);
+      setMessage("Error while verification. Please try again later");
+    }
+  };
+
+  return (
+    <div className="container mt-4 d-flex justify-content-center">
+      <div className="card" style={{ width: 500 }}>
+        <div className="card-header">User Profile</div>
+        <div className="card-body">
+          <p className={msg ? "" : "mb-0"}>{msg}</p>
+          <div className="d-flex justify-content-between">
+            <h4>
+              Name - {user.name}{" "}
+              {cookie.logged_user * 1 === user.id * 1 ? "(me)" : ""}
+            </h4>
+            {cookie.logged_user * 1 !== user.id * 1 && !user.verified ? (
+              <button
+                className="btn btn-success"
+                disabled={disable}
+                onClick={verifyUser}
+              >
+                Verify
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
+          <p>Email - {user.email}</p>
+          <h5>
+            <span
+              className={
+                user.verified ? "bg-success badge" : "bg-warning badge"
+              }
+            >
+              {user.verified ? "Verified Profile" : "Pending verification"}
+            </span>
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+}
